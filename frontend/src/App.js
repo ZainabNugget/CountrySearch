81472a530cadb1f9a680d0fb915287070ce61efa
ZainabNugget <142988019+ZainabNugget@users.noreply.github.com>
2023-10-16 16:25:51 +0100

    Styling added
    For this update, styling was added to app.css, and added two images and a small feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,61 +21,63 @@ function App() {
       }
       const data = await response.json(); //put all data into this variable
       setCountryData(data); //set it into this data variable
-      console.log(data)
     } catch (error) {
       console.error('Error:', error); //print out any occuring errors :)
     }
   };
 
   function cleanse(data) {
-      data.replaceAll('{',' ').replaceAll('}',' ').replaceAll('"',' ');
+    return data.replaceAll('{',' ').replaceAll('}',' ').replaceAll('"',' ');
   }
 
   return (
     <div id='page'>
       {/* Below this will be the search bar, where the user will input the country */}
-      <section className="searchBar">
-        <h3 id='greetings'>Greetings! Search your country!</h3>
+        <section id="searchBar" >
+        <h3 id='greetings' className='flex-row'>Greetings! Search your country!</h3>
+        <div id='earth'></div>
         <input
           id='search'
           type='text'
           value={userString}
           onChange={handleChange}
+          className='z99'
           placeholder="Type Your Country" />
+          {/* After this button is pressed, the country will be processed, then the information will be displayed */}
+        <button id='btn' onClick={fetchData}>Search</button>
       </section>
-      {/* After this button is pressed, the country will be processed, then the information will be displayed */}
-      <button id='btn' onClick={fetchData}>Search</button>
       {/* Below here, in the article section, info about the country will be placed */}
-      <p>We found {countryData.length} result(s)</p>
+      {/* <p>We found {countryData.length} result(s)</p> */}
       {Array.isArray(countryData) && countryData.length > 0 ? (
         (countryData.map((data, index) => (
-          <article id='info-page'  key={index} >
-          <section id='info-header'>
+          <article id='info-page' className='flex-col' key={index} >
+          <section id='info-header' className='flex-col'>
             <h1>{data.name.common}</h1>
             <img alt='flag of country' src={data.flags["png"]}/>
           </section>
-          <section id='general-info'>
-            <p>The capital of {data.name.common} is {data.capital}</p>
-            <p>with a population of: {data.population}</p>
-            <p>It is located in {data.region}</p>
-            <p>In the subregion {data.subregion}</p>
-            <p>Other ways of naming this country: {data.altSpellings+ " "}</p>
-            <p>{data.flags["alt"]}</p>
-            <p>The week starts on a {data.startOfWeek}</p>
-            <p>The currencies of this country: {JSON.stringify(data.currencies)}</p>
-            <p>Languages that this country speak, </p>
+          <section id='general-info' className=''>
+          <p id='text'>~ Other ways of naming this country are {data.altSpellings+ " "} 
+          <br />~ The capital of {data.name.common} is {data.capital} with a population of: {data.population} people... 
+          <br/>~ It is located in {data.region}, In the subregion {data.subregion} 
+          <br/>~ {data.flags["alt"]}
+          <br/>~ The week starts on a {data.startOfWeek} 
+          <br />~ The currencies of this country: {cleanse(JSON.stringify(data.currencies))}, 
+          <br/>~ Languages that this country speak, {cleanse(JSON.stringify(data.languages))}
+          <br/>~ 
             {data.independent ? (
-                <p>This country is independent</p>
+                <span>This country is independent</span>
             ):(
-              <p>This country is not independent</p>
+              <span>This country is not independent</span>
             )}
+            <br/>For more information on this country, follow the <a href={'https://en.wikipedia.org/wiki/'+data.name.common} >wikipedia page</a>!
+            </p>
+            <img id='img' src={data.coatOfArms["png"]}/>
           </section>
         </article>
       )))
       ):(
-        <p>Try searching for a country!</p>
+        <p className='flex-row'>Try searching for a country!</p>
       )}
-      
     </div>
   );
 }
